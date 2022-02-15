0eb6aeeb47c1417dcf9afa7132b9ae5ef62fffe7
skshabeera <82590734+skshabeera@users.noreply.github.com>
2022-02-15 17:12:34 +0530

    Add files via upload

--- bubblesort.py
+++ bubblesort.py
@@ -1,13 +1,12 @@
+
+
 a=[10,2,3,6]
 i=0
-attempt=0
 while i<len(a):
-	j=0
-	while j<len(a):
-		if a[i]<a[j]:
-			attempt=a[j]
-			a[j]=a[i]
-			a[i]=attempt
-		j=j+1
-	i=i+1
+    j=0
+    while j<len(a)-1-i:
+        if a[j]>a[j+1]:
+            a[j],a[j+1]=a[j+1],a[j]
+        j=j+1
+    i=i+1
 print(a)
